57e72d7aa3b8414fee0fdad38b4d845c05021cae
Christian Stewart <christian@aperture.us>
2026-03-27 18:49:52 -0700

    feat(v86fs): add filesystem registration and root inode
    register_filesystem(v86fs) with get_tree_nodev. fill_super creates root
    inode with S_IFDIR|0755, simple_dir_operations, simple_lookup. mount -t
    v86fs none /mnt now succeeds with an empty directory.
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- fs/v86fs/super.c
+++ fs/v86fs/super.c
@@ -9,11 +9,16 @@
  */
 
 #include <linux/fs.h>
+#include <linux/fs_context.h>
 #include <linux/module.h>
+#include <linux/pagemap.h>
+#include <linux/slab.h>
+#include <linux/statfs.h>
 #include <linux/virtio.h>
 #include <linux/virtio_config.h>
 
 #define V86FS_VIRTIO_ID 63
+#define V86FS_MAGIC     0x56383646 /* "V86F" */
 
 #define V86FS_VQ_HIPRIQ   0
 #define V86FS_VQ_REQUESTQ 1
@@ -25,19 +30,112 @@ struct v86fs_device {
 	struct virtqueue *vqs[V86FS_VQ_MAX];
 };
 
+/* Global device pointer (single device supported for now) */
+static struct v86fs_device *v86fs_dev;
+
+/*
+ * VFS operations
+ */
+
+static const struct super_operations v86fs_super_ops = {
+	.statfs		= simple_statfs,
+
+};
+
+static const struct inode_operations v86fs_dir_inode_ops = {
+	.lookup		= simple_lookup,
+};
+
+
+static struct inode *v86fs_make_inode(struct super_block *sb, umode_t mode)
+{
+	struct inode *inode;
+
+	inode = new_inode(sb);
+	if (!inode)
+		return NULL;
+
+	inode->i_ino = get_next_ino();
+	inode->i_mode = mode;
+	simple_inode_init_ts(inode);
+
+	switch (mode & S_IFMT) {
+	case S_IFDIR:
+		inode->i_op = &v86fs_dir_inode_ops;
+		inode->i_fop = &simple_dir_operations;
+		inc_nlink(inode);
+		break;
+	case S_IFREG:
+		inode->i_fop = &simple_dir_operations;
+		break;
+	}
+
+	return inode;
+}
+
+static int v86fs_fill_super(struct super_block *sb, struct fs_context *fc)
+{
+	struct inode *root_inode;
+
+	sb->s_maxbytes	= MAX_LFS_FILESIZE;
+	sb->s_blocksize	= PAGE_SIZE;
+	sb->s_blocksize_bits = PAGE_SHIFT;
+	sb->s_magic	= V86FS_MAGIC;
+	sb->s_op	= &v86fs_super_ops;
+	sb->s_time_gran	= 1;
+
+	root_inode = v86fs_make_inode(sb, S_IFDIR | 0755);
+	if (!root_inode)
+		return -ENOMEM;
+
+	sb->s_root = d_make_root(root_inode);
+	if (!sb->s_root)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static int v86fs_get_tree(struct fs_context *fc)
+{
+	return get_tree_nodev(fc, v86fs_fill_super);
+}
+
+static const struct fs_context_operations v86fs_context_ops = {
+	.get_tree	= v86fs_get_tree,
+};
+
+static int v86fs_init_fs_context(struct fs_context *fc)
+{
+	fc->ops = &v86fs_context_ops;
+	return 0;
+}
+
+static void v86fs_kill_sb(struct super_block *sb)
+{
+	kill_anon_super(sb);
+}
+
+static struct file_system_type v86fs_type = {
+	.name		= "v86fs",
+	.init_fs_context = v86fs_init_fs_context,
+	.kill_sb	= v86fs_kill_sb,
+	.owner		= THIS_MODULE,
+};
+
+/*
+ * Virtio device
+ */
+
 static void v86fs_hipriq_cb(struct virtqueue *vq)
 {
-	pr_debug("v86fs: hipriq callback\n");
 }
 
 static void v86fs_requestq_cb(struct virtqueue *vq)
 {
-	pr_debug("v86fs: requestq callback\n");
 }
 
 static void v86fs_notifyq_cb(struct virtqueue *vq)
 {
-	pr_debug("v86fs: notifyq callback\n");
 }
 
 static int v86fs_init_vqs(struct v86fs_device *v86dev)
@@ -86,6 +184,7 @@ static int v86fs_probe(struct virtio_device *vdev)
 	}
 
 	virtio_device_ready(vdev);
+	v86fs_dev = v86dev;
 
 	pr_info("v86fs: probed, %d virtqueues ready\n", V86FS_VQ_MAX);
 	return 0;
@@ -99,6 +198,7 @@ static void v86fs_remove(struct virtio_device *vdev)
 {
 	struct v86fs_device *v86dev = vdev->priv;
 
+	v86fs_dev = NULL;
 	vdev->config->reset(vdev);
 	vdev->config->del_vqs(vdev);
 	kfree(v86dev);
@@ -119,11 +219,25 @@ static struct virtio_driver v86fs_driver = {
 
 static int __init v86fs_init(void)
 {
-	return register_virtio_driver(&v86fs_driver);
+	int err;
+
+	err = register_virtio_driver(&v86fs_driver);
+	if (err)
+		return err;
+
+	err = register_filesystem(&v86fs_type);
+	if (err) {
+		unregister_virtio_driver(&v86fs_driver);
+		return err;
+	}
+
+	pr_info("v86fs: registered\n");
+	return 0;
 }
 
 static void __exit v86fs_exit(void)
 {
+	unregister_filesystem(&v86fs_type);
 	unregister_virtio_driver(&v86fs_driver);
 }
 
